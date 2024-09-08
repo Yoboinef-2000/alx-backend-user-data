8aedbd8f7d0c6589d24b0c62d25029ab8a93c310
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-08 17:15:03 -0400

    Obfuscate the log message.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,30 @@
+#!/usr/bin/env python3
+
+"""Log and filter data"""
 import logging
+from typing import List, Tuple
+import os
+import re
+# import mysql.connector
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscate the log message."""
+    pattern = r"|".join([f"(?<={field}=)[^{separator}]+" for field in fields])
+    return re.sub(pattern, redaction, message)
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    SEPARATOR = ""
 
     def __init__(self):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
