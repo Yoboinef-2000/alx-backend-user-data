827050a6a2522acf7dfb51a93f6ce341c6afff43
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-11 14:12:31 -0400

    Fixed the error.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Class User."""
+"""Class User.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,10 +9,12 @@ Base = declarative_base()
 
 
 class User(Base):
-    """User class."""
+    """User class.
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
+    reset_token = Column(String(250), nullable=True)
