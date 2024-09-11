a1c4573946fe27c686ee0604b7563b7b65edc901
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-11 14:07:47 -0400

    Adding a new user.

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from sqlalchemy.orm.session import Session
+from user import User  # Assuming you already have the User model defined
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database and return the User object"""
+        # Create a new User instance
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        # Add the new user to the session
+        self._session.add(new_user)
+
+        # Commit the transaction to save the new user to the database
+        self._session.commit()
+
+        # Return the newly created user
+        return new_user
