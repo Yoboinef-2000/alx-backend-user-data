29fe821649333426fc255b7d7ff0820d9926d637
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-09-12 16:33:53 -0400

    Auth Module.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""The Auth Module.
+"""The Auth Module
 """
 
 import bcrypt
-from db import DB
-from sqlalchemy.orm.exc import NoResultFound
-from user import User
 from uuid import uuid4
 from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
